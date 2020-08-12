5ce9198483a34d05624245da6c2ba3d87b866b9c
Łukasz Łyskawa <lukasz.lyskawa@finanteq.com>
2020-08-12 12:46:43 +0200

    Add bouncy castle license

--- plugin/src/main/java/com/cookpad/android/plugin/license/data/LibraryInfo.kt
+++ plugin/src/main/java/com/cookpad/android/plugin/license/data/LibraryInfo.kt
@@ -66,6 +66,7 @@ data class LibraryInfo(
             mopub_sdk_license_a.matches(license)
                     || mopub_sdk_license_b.matches(license) -> "mopub_sdk_license"
             android_software_license.matches(license) -> "android_software_development_kit_license"
+            bouncy_castle_license.matches(license) -> "bouncy_castle_license"
             else -> license
         }
     }
@@ -134,5 +135,6 @@ data class LibraryInfo(
         val mopub_sdk_license_a = """(?i).*\bmopub\b.*""".toRegex()
         val mopub_sdk_license_b = """(?i).*\bmopub\b.*\bsdk\b.*\blicense\b.*""".toRegex()
         val android_software_license = """(?i).*\bAndroid.*\bSoftware.*\bDevelopment.*\bKit.*\bLicense\b.*""".toRegex()
+        val bouncy_castle_license = """(?i).*\bBouncy.*\bCastle.*\bLicence\b.*""".toRegex()
     }
 }
